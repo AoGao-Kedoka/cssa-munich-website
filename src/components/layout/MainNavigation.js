36e3739564b12cb5963b242d3c406c64a5adce9c
Ao Gao <ao.gao@tum.de>
2022-04-05 15:55:45 +0200

    Update header

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,48 @@
+import { Link, Outlet } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav} from "react-bootstrap";
+
+
+const MainNavigation = () => {
+	return (
+		<header>
+			<Navbar bg="light">
+				<Container>
+					<Navbar.Brand href="/" className="mx-auto">
+						<img
+							src="/logo192.jpg"
+							className="profile-header"
+							alt="CSSA Logo"
+						/>
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav className="w-100 nav-justified">
+							<Nav.Item>
+								<Nav.Link as={Link} to="/career">职业宣讲</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link as={Link} to="/academic">学术论坛</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link as={Link} to="/culture_and_sports">文体活动</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link as={Link} to="/student">学生服务</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link as={Link} to="/cssa">学联专栏</Nav.Link>
+							</Nav.Item>
+							<Nav.Item>
+								<Nav.Link as={Link} to="/about_us">关于我们</Nav.Link>
+							</Nav.Item>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<Outlet />
+		</header>
+	);
+}
+
+export default MainNavigation;
