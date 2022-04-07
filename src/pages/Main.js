3d497211e14259c30fc57c20b33f45b2182dea1d
Ao Gao <ao.gao@tum.de>
2022-04-07 15:04:04 +0200

    Update card structure

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import CardNavigation from "../components/layout/CardNavigation";
+
+const MainPage = () => {
+	return (
+        <Container>
+            <CardNavigation />
+        </Container>
+    )
+}
+
+export default MainPage;
