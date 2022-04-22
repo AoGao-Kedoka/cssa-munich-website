fbc1eee7b02e9a88f6292cba711e75d8c10934e3
Ao Gao <ao.gao@tum.de>
2022-04-22 15:30:42 +0200

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,18 @@ import "./Footer.css"
 
 const Footer = () => {
 	return (
-		<div style={{"padding-top":"60px"}}>
-			<div className="footer_border">
-				<h1 className="partner">
-					合作伙伴
-				</h1>
-				<span className="rights_cssa">
-				2022 © CSSA Munich ALL Rights Reserved. Privacy Policy | Terms of Service
-				</span>
+		<footer>
+			<div style={{"padding-top":"60px"}}>
+				<div className="footer_border">
+					<h1 className="partner">
+						合作伙伴
+					</h1>
+					<span className="rights_cssa">
+					2022 © CSSA Munich ALL Rights Reserved. Privacy Policy | Terms of Service
+					</span>
+				</div>
 			</div>
-		</div>
+		</footer>
 	)
 }
 
