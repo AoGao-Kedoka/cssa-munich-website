14b4cbab4a3118b9ddc68140be0ca15cee94495f
Ao Gao <ao.gao@tum.de>
2022-05-26 12:46:13 +0200

    Finish fetching data

--- src/api/FetchData.js
+++ src/api/FetchData.js
@@ -1,20 +1,16 @@
 import { useState, useEffect } from "react";
 
-const useFetch = ( eventType) => {
-	const[data, setData] = useState(null);
-	const[error, setError] = useState(null);
-	const[loading, setLoading] = useState(true);
-	const url = "http://cssa-munich.de:1337/api/events?populate=MainImage"
+const useFetch = (rootUrl, type) => {
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		const fetchData = async() => {
+		const fetchData = async () => {
 			setLoading(true);
-			try{
-				const res = await fetch(url);
+			try {
+				const res = await fetch(rootUrl + "/api/events?filters[EventType][$eq]="+ type + '&populate=MainImage');
 				const json = await res.json();
-				//https://strapi.io/blog/deep-filtering-alpha-26
-				//https://docs.strapi.io/developer-docs/latest/developer-resources/database-apis-reference/rest/filtering-locale-publication.html#filtering
-				//TODO
 				setData(json);
 			} catch (error) {
 				setError(error);
@@ -23,8 +19,8 @@ const useFetch = ( eventType) => {
 		}
 
 		fetchData();
-	}, [eventType])
-	return {loading, error, data};
+	}, [rootUrl, type])
+	return { loading, error, data };
 }
 
 export default useFetch;
