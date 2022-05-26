60ad2c25422fd3243e81fcbe74e1393c63595c43
Ao Gao <ao.gao@tum.de>
2022-05-26 12:46:13 +0200

    Finish fetching data

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,9 +2,9 @@ import "./Events.css"
 import useFetch from "../api/FetchData";
 import { Card } from "react-bootstrap";
 
-const Events = (type) => {
+const Events = (props) => {
 	const rootUrl = "http://cssa-munich.de:1337";
-	const { loading, error, data } = useFetch("CSSA");
+	const { loading, error, data } = useFetch(rootUrl, props.type);
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error!</p>;
 	
@@ -12,7 +12,7 @@ const Events = (type) => {
 		<dir style={{paddingTop:"100px"}}>
 			{data.data.map(cssaEvents => (
 				<div key={cssaEvents.id}>
-					<Card className="justify-content-center event-card">
+					<Card className="event-card">
 						<Card.Img className="img-card" src={rootUrl + cssaEvents.attributes.MainImage.data.attributes.url} />
 						<Card.Body>
 							<Card.Title>{cssaEvents.attributes.Title}</Card.Title>
--- src/pages/AcademicForum.js
+++ src/pages/AcademicForum.js
@@ -1,9 +1,11 @@
 import Pinned from "../components/Pinned";
+import Events from "../components/Events";
 
 const AcademicForumPage = () => {
 	return (
     <dir>
       <Pinned />
+      <Events type="AcademicForum" />
     </dir>
   );
 }
