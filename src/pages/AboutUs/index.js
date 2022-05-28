2c986cbe72f04a35ab9f9c3c425184123ce77e9c
Yanjie L <jasli1999@outlook.com>
2022-05-28 17:53:51 +0200

    FEAT: account links

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,11 +1,13 @@
 import Intro from './Intro';
 import Departments from './Departments';
+import Accounts from './Accounts';
 
 const AboutUsPage = () => {
 	return (
         <div style={{textAlign: 'center'}}>
             <Intro />
             <Departments />
+            <Accounts />
         </div>
     )
 }
