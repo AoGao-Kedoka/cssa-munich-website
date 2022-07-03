9b7907b0b709e867f15ef074b72d08e70844f0cb
YSheepYY <yang20001117@gmail.com>
2022-07-03 12:27:26 +0200

    hard coded card elements finish

--- src/pages/CSSA.js
+++ src/pages/CSSA.js
@@ -2,19 +2,19 @@ import Pinned from "../components/Pinned";
 import Events from "../components/Events";
 import bigCardImage from "../resources/images/CSSACover.jpeg";
 import leftCardImage1 from "../resources/images/CSSACover1.jpeg";
-import rightCardImage1 from "../resources/images/CSSACover2.jpeg";
-import leftCardImage2 from "../resources/images/CSSACover3.gif";
-import rightCardImage2 from "../resources/images/CSSACover4.jpeg";
+import rightCardImage1 from "../resources/images/CSSACover2.jpg";
+import leftCardImage2 from "../resources/images/CSSACover3.jpeg";
+import rightCardImage2 from "../resources/images/culturalCover2.jpg";
 
 const CSSAPage = () => {
     return (
         <dir>
             <Pinned
-                bigCardTitle="Das chinesische Neujahrsfest" bigCardDate="2022-02-03" bigCardImage={bigCardImage}
-                leftCardTitle1="“Dein Weihnachtsmann ist da | Zusammenfassung der Veranstaltung: Es ist erfreulicher zu teilen als zu bekommen" leftCardDate1="2022-01-07" leftCardImage1={leftCardImage1}
-                rightCardTitle1="CSSA X AES丨Kulturaustauschtreffen zwischen China und Europa erfolgreich abgeschlossen" rightCardDate1="2021-12-27" rightCardImage1={rightCardImage1}
-                leftCardTitle2="„Light up, the way to love“: Zusammenfassung der Veranstaltung „die CP Woche“ in München + Berlin" leftCardDate2="2022-05-16" leftCardImage2={leftCardImage2}
-                rightCardTitle2="2022夏季学期学联纳新圆满收官" rightCardDate2="2022-05-12" rightCardImage2={rightCardImage2}/>
+                bigCardTitle="如何在慕尼黑延长学生签证" bigCardDate="2022-04-28" bigCardImage={bigCardImage}
+                leftCardTitle1="“9欧月票”已开放购买！你想知道的全在这里！" leftCardDate1="2022-05-24" leftCardImage1={leftCardImage1}
+                rightCardTitle1="慕尼黑HPV疫苗超全接种攻略~" rightCardDate1="2022-06-12" rightCardImage1={rightCardImage1}
+                leftCardTitle2="新鲜出炉的德国驾照攻略！" leftCardDate2="2022-05-27" leftCardImage2={leftCardImage2}
+                rightCardTitle2="'Cool Young' Basketball-Freundschaftsspiel der CSSA München erfolgreich abgeschlossen" rightCardDate2="2022-04-08" rightCardImage2={rightCardImage2}/>
             />
             <Events type="CSSA"/>
         </dir>
