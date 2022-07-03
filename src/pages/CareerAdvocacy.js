d23eac215087749971874322cd0c5c271f25f1fc
YSheepYY <yang20001117@gmail.com>
2022-07-03 14:03:27 +0200

    small bug

--- src/pages/CareerAdvocacy.js
+++ src/pages/CareerAdvocacy.js
@@ -15,7 +15,6 @@ const CareerAdvocacyPage = () => {
                 rightCardTitle1="投行就业指导讲座圆满结束" rightCardDate1="2022-05-14" rightCardImage1={rightCardImage1}
                 leftCardTitle2="2021年慕尼黑学联职业养成训练营第1期圆满完成！" leftCardDate2="2021-03-17" leftCardImage2={leftCardImage2}
                 rightCardTitle2="华为慕尼黑站线下“圆桌派”圆满结束" rightCardDate2="2021-11-28" rightCardImage2={rightCardImage2}/>
-            />
             <Events type="CareerAdvocacy"/>
         </dir>
     );
